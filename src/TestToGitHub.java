41f63675837f13ca45aaf91b825c1495a4220761
AvaXiaohongDeng <xiaohongdeng196@gmail.com>
2020-11-19 13:22:51 -0500

    Test if Intellij IDEA connects to GitHub

---
+++ src/TestToGitHub.java
@@ -0,0 +1,12 @@
+
+/* Name:  Xiaohong Deng(Ava)
+ * Assignment:  Question 1
+
+ * Displays hello world
+ */
+
+public class TestToGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub!");
+    }
+}
